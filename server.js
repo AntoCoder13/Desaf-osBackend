1c016ed29b6b1a934134f72a337d3277b6ce9415
AntoCoder13 <antonellaargisanchez@gmail.com>
2024-01-16 19:25:19 -0300

    agregándole async/await a Servidores

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const express = require('node:express')
 const port = 3000
 
+class Products {
+    constructor() {
+    }
+  
+    async get() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve([
+            { id: 1, name: 'Product 1' },
+            { id: 2, name: 'Product 2' },
+          ]);
+        }, 2000);
+      });
+    }
+  
+    async find(pid) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          const products = [
+            { id: 1, name: 'Product 1' },
+            { id: 2, name: 'Product 2' },
+          ];
+  
+          const product = products.find(p => p.id === parseInt(pid));
+          resolve(product);
+        }, 2000);
+      });
+    }
+  }
+
 app.get('/products', (req, res) => {
     const { limit } = req.query
     const productsManager = new Products()
